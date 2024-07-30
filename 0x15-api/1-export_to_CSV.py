f6b79ca43b46f7273e55cf24b90dba2f929d9110
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-07-29 19:50:45 -0700

    commiting answers on API's

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Consuming my first REST API for todo lists of employees and storing the data in a CSV file"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+Fetching my first APIs
